0d66d3f33efc8de9ed1433c22dd6e87b9db7b32a
Fedorov Valentyn <fedorovvalentin843@gmail.com>
2023-09-21 21:27:46 +0300

    First commit

--- Main/main.cpp
+++ Main/main.cpp
@@ -1,13 +1,16 @@
-#include<iostream>
-#include<ctime>
+#include <iostream>
 
 using namespace std;
+
 int main() {
-
-
-	cout << "Hello world" << endl;
-
-
-
-	return 0;
+    string name;
+    cout << "Enter your name:" << endl;
+    cin >> name;
+    cout << "My name is " << name  << "." << endl;
+    cout << "Hello " << name << ". My name is Tom Riddle." << endl;
+    cout << "Do you know anything about the Chamber of Secrets?" << endl;
+    cout << "Yes." << endl;
+    cout << "Can you tell my?" << endl;
+    cout << "No. But I can show you." << endl;
+    return 0;
 }
