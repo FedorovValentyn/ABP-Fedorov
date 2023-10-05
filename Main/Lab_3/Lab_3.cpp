c1af80446c8af0a09bafc42216b854c731f023b1
Fedorov Valentyn <fedorovvalentin843@gmail.com>
2023-10-05 17:27:55 +0300

    Lab №3

--- Main/Lab_3/Lab_3.cpp
+++ Main/Lab_3/Lab_3.cpp
@@ -6,14 +6,15 @@ using namespace std;
 
 
 int Lab_3(){
-    long n, k2 = 1;
+    long  k2 = 1;
     double dbln, sum=0, term;
     const double EPS = 0.000001;
     short k1= 1;
+    int n;
 
-    for (int n = 0; ;n++, k2 *=2, k1 =-k1) {
+    for (n = 0; ;n++, k2 *=2, k1 = -1*k1) {
         dbln = static_cast<double>(n);
-        term = k1 * ((2 * dbln + 3) / (dbln + k2) * (dbln + k2));
+        term = k1 * (1 - ((dbln+1)*(dbln +1))/((dbln+2)*(dbln+2)));
         if (fabs(term)>=EPS){
             sum +=term;
         }
@@ -25,5 +26,6 @@ int Lab_3(){
         }
     }
     cout<<"Повна сума ряду: "<< fixed<<setprecision(6)<< sum<<endl;
+    cout<<"Ітерація: "<< n << endl;
     return 0;
 }
